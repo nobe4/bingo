eef8ea7eb8b1da76cfaa417eb3d7beb686686962
nobe4 <nobe4@users.noreply.github.com>
2023-12-16 14:25:10 +0100

    feat(save): add full save in URL and start loading

--- script.js
+++ script.js
@@ -23,9 +23,50 @@ function edit(is_edit) {
   });
 }
 
-function test(v) {
-  LZMA.compress(v, 9, function (compressed, error) {
-    result.innerText = compressed;
+function load() {
+  let u = new URL(location.toString());
+  try {
+    data = u.searchParams.get("d").split(",").map(Number);
+    LZMA.decompress(data, function (d, e) {
+      if (e) {
+        console.error(e);
+      }
+
+      d = JSON.parse(d);
+
+      size = d.size;
+
+      generate();
+
+      console.log(d);
+      Array.from(main.children).forEach((n, i) => {
+        n.innerText = d.nodes[i].text;
+        n.toggleAttribute("checked", d.nodes[i].checked);
+      });
+    });
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function save() {
+  let data = JSON.stringify({
+    size: size,
+    nodes: Array.from(main.children).map((n) => {
+      return {
+        text: n.innerText,
+        checked: n.getAttribute("checked") === "",
+      };
+    }),
+  });
+
+  LZMA.compress(data, 9, function (compressed, e) {
+    if (e) {
+      console.error(e);
+    }
+    let u = new URL(location.toString());
+    u.searchParams.set("d", compressed);
+    history.pushState(null, "", u.toString());
   });
 }
 
